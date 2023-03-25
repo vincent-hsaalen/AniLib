01c982b1debd91241c6708497af4b1c974d74c6a
Vincent <vincent.burkhardt@studmail.htw-aalen.de>
2023-03-25 23:24:53 +0100

    added basic startup screen without any functionality (yet)

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -4,6 +4,9 @@ namespace AniLib;
 
 public partial class MainWindow : Window{
     public MainWindow(){
+        this.MinWidth = 720;
+        this.MinHeight = 480;
+        this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         InitializeComponent();
     }
 }
